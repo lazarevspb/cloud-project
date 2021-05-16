60c4ce4e331b2a1b5c44a257c5123092ff70091b
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-05-16 13:19:40 +0300

    init commit

---
+++ src/test/java/ru/lazarev/cloud/project/CloudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lazarev.cloud.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
